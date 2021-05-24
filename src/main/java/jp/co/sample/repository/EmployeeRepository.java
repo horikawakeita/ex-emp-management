2d60d321433750d92f9c201ade6a1911a8f78d92
horikawakeita <keita.horikawa@rakus.co.jp>
2021-05-24 12:26:19 +0900

    updateのsql文でスペースが抜けている箇所があったため修正

--- src/main/java/jp/co/sample/repository/EmployeeRepository.java
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -40,7 +40,7 @@ public class EmployeeRepository {
 		String sql = "UPDATE employees SET name=:name,image=:image,gender=:gender,"
 				+ "hire_date=:hireDate,mail_address=:mailAddress,zip_code=:zipCode,address=:address,"
 				+ "telephone=:telephone,salary=:salary,characteristics=:characteristics,"
-				+ "dependents_count=:dependentsCount" + "WHERE id=:id";
+				+ "dependents_count=:dependentsCount" + " WHERE id=:id";
 
 		template.update(sql, param);
 	}
